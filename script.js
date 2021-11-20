69a67d9a194680af897f04b313abab4b98d4a5b4
Ramandeep9898 <ramank6238299@gmail.com>
2021-11-20 19:22:09 +0530

    initial Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var txtInput = document.querySelector("#txtInput");
+var btnTranslate = document.querySelector("#btnTranslate");
+var divOutput = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(text)
+{
+    return serverUrl + "?text=" + text;
+}
+
+function errorHandler(error)
+{
+    console.log(error);
+    alert("Something is wrong with the server! Please try after sometime");
+}
+
+function clickHandlerOfTranslate()
+{
+    var inputValue = txtInput.value;
+    fetch(getTranslationUrl(inputValue)).then(Response => Response.json()).then(json => divOutput.textContent = json.contents.translated).catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickHandlerOfTranslate);
